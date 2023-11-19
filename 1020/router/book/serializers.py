de52d5fc4896fab42c65c216749af5734e8b2599
LeeCH <ckdgus2323@naver.com>
2023-11-19 23:40:22 +0900

    1020

--- 1020/router/book/serializers.py
+++ 1020/router/book/serializers.py
@@ -4,4 +4,5 @@ from .models import Book
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
-        fields = '__all__'
+        # fields = '__all__'
+        fields = '__title__'
