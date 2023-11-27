dbb607600f15b535eafeab57bfc5628e1056b17a
LeeCH <ckdgus2323@naver.com>
2023-11-27 23:57:16 +0900

    1120

--- 1120/DRF_training/blog/serializers.py
+++ 1120/DRF_training/blog/serializers.py
@@ -3,19 +3,19 @@ from .models import Postblog
 from django.contrib.auth import get_user_model
 
 
-# class AuthorSerializer(ModelSerializer):
-#     class Meta:
-#         model = get_user_model()
-#         fields = ['username', 'email']
+class AuthorSerializer(ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ['username', 'email']
 
 
 class PostSerializer(ModelSerializer):
-    # author = AuthorSerializer()
+    author = AuthorSerializer()
     class Meta:
         model = Postblog
         fields = [
             'id',
-            # 'author',
+            'author',
             'title',
             'content',
             'created_at',
