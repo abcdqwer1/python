4a78719c4c9ade37efd3c23e156a7eee5024085b
abcdqwer1 <ckdgus2323@naver.com>
2023-12-07 15:18:50 +0900

    1207

---
+++ 1207/users/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import (
+    UserCreateView,
+    UserDetailView,
+)
+
+# login과 logout 등의 url을 구현하지 않는 이유는 JWT을 사용하기 때문입니다.
+urlpatterns = [
+    path('signup/', UserCreateView.as_view(), name='signup'),
+    path('user/<int:pk>/', UserDetailView.as_view(), name='user_detail'),
+]
